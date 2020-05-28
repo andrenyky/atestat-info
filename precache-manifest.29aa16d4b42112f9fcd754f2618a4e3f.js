49a55152a45854fee7ee21313af0bf9d5e041f02
andrenyky <46795046+andrenyky@users.noreply.github.com>
2020-05-28 15:42:13 +0300

    Updates

---
+++ precache-manifest.29aa16d4b42112f9fcd754f2618a4e3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad201bb1a4f42a3cad3bddf583548e16",
+    "url": "/atestat-info/index.html"
+  },
+  {
+    "revision": "e2128e034366b7479540",
+    "url": "/atestat-info/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "2636df2a61c8d2276d2f",
+    "url": "/atestat-info/static/css/main.359ce6db.chunk.css"
+  },
+  {
+    "revision": "e2128e034366b7479540",
+    "url": "/atestat-info/static/js/2.e00f7cba.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/atestat-info/static/js/2.e00f7cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2636df2a61c8d2276d2f",
+    "url": "/atestat-info/static/js/main.cd3de807.chunk.js"
+  },
+  {
+    "revision": "bbd23c634fed01b14f53",
+    "url": "/atestat-info/static/js/runtime-main.630f9566.js"
+  }
+]);
